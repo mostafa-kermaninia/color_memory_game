06a429b8d9c6fb80239e9452b47247ee090dc53a
mosiyo <mkermani1383@gmail.com>
2025-07-23 00:02:09 +0330

    Update index.js

--- backend/DataBase/models/index.js
+++ backend/DataBase/models/index.js
@@ -1,19 +1,32 @@
 'use strict';
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 const fs = require('fs');
+const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../../config/config.json')[env];
 const db = {};
 
-let sequelize = new Sequelize(config.database, config.username, config.password, config);
+// --- تغییر اصلی اینجاست ---
+// اتصال مستقیم به دیتابیس با استفاده از آدرس کامل
+const sequelize = new Sequelize(
+  'mysql://colormemory_user:13831383@localhost:3306/colormemory_db',
+  {
+    dialect: 'mysql',
+    logging: console.log // لاگ کردن کوئری‌های دیتابیس برای دیباگ
+  }
+);
+// --- پایان تغییر ---
 
 fs
   .readdirSync(__dirname)
-  .filter(file => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
+  })
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
