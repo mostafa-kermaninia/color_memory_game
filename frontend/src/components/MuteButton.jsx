ec7963ed23c67745c7c85c6882a7e9ad8b0daad7
mosiyo <mkermani1383@gmail.com>
2025-08-09 19:04:38 +0330

    Update MuteButton.jsx

--- frontend/src/components/MuteButton.jsx
+++ frontend/src/components/MuteButton.jsx
@@ -1,7 +1,8 @@
 // frontend/src/components/MuteButton.jsx
 
 import React from 'react';
-import { motion } from 'framer-motion';
+
+import { motion, AnimatePresence } from 'framer-motion';
 
 const SoundOnIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
